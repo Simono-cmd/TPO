150261121a2ef4ee887a711915b2901b031349ac
Szymon <trauth.szymon@gmail.com>
2025-04-18 21:15:22 +0200

    Client

--- TPO4_TS_S30749/src/zad1/Client.java
+++ TPO4_TS_S30749/src/zad1/Client.java
@@ -4,44 +4,71 @@
  *
  */
 
+
 package zad1;
-
-
-public class Client {
-    public Client(String host, int port, String id) {
-    }
-
-    public void connect() {
-    }
-
-    public String send(String s) {
-        return null;
-    }
-}
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 
+public class Client
+{
+    private final String host;
+    private final int port;
+    private final String id;
+    private SocketChannel clientChannel;
 
-public class Client {
-    public static void main(String[] args) {
+    public Client(String host, int port, String id) {
+        this.host = host;
+        this.port = port;
+        this.id = id;
+    }
 
-        try (
-                SocketChannel socketChannel = SocketChannel.open();
-        ) {
-            socketChannel.connect(new InetSocketAddress("localhost", 7777));
-            socketChannel.configureBlocking(false);
-
-            socketChannel.write(ByteBuffer.wrap("Hello".getBytes( StandardCharsets.UTF_8)));
-
-            socketChannel.socket().getOutputStream().flush();
-            socketChannel.socket().close();
+    public void connect()
+    {
+        try
+        {
+            clientChannel = SocketChannel.open();
+            clientChannel.configureBlocking(false);
+            clientChannel.connect(new InetSocketAddress(host, port));
+            while (!clientChannel.finishConnect()) {}
         }
-        catch (IOException e) {
-            throw new RuntimeException(e);
+        catch (IOException e)
+        {
+            throw new RuntimeException("Error connecting to " + host + ":" + port, e);
         }
+    }
 
+    public String send(String req)
+    {
+        try {
+            ByteBuffer writeBuffer = ByteBuffer.wrap((req + '\n').getBytes(StandardCharsets.UTF_8));
+            while (writeBuffer.hasRemaining())
+            {
+                clientChannel.write(writeBuffer);
+            }
+
+            ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+            int bytesRead = 0;
+            while (bytesRead == 0)
+            {
+                bytesRead = clientChannel.read(readBuffer);
+                if (bytesRead == -1)
+                {
+                    throw new RuntimeException("Connection closed");
+                }
+            }
+            readBuffer.flip();
+            return StandardCharsets.UTF_8.decode(readBuffer).toString().trim();
+        } catch (IOException e)
+        {
+            throw new RuntimeException("Error communicating with the server: ", e);
+        }
+    }
+
+    public String getId()
+    {
+        return id;
     }
 }
